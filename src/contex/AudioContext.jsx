96016e473d3e8f2fdf4c3daab3b662c8d9921fa5
vanya470 <83009040+vanya470@users.noreply.github.com>
2024-01-06 01:19:47 +0300

    Add files via upload

---
+++ src/contex/AudioContext.jsx
@@ -0,0 +1,62 @@
+// import { createContext, useState } from "react";
+// import tracksList from "../assets/tracksList";
+
+// export const AudioContex = createContext({});
+
+// const AudioProvider = ({ children }) => {
+//     const [currentTrack, setCurrentTrack] = useState(tracksList[0]);
+//     const [isPlaying, setPlaying] = useState(false);
+
+//     const handleToggleAudio = (track) => {
+//         console.log("Клик!");
+//     };
+
+//     const value = { currentTrack, isPlaying, handleToggleAudio };
+
+//     return <AudioContext.Provider value={value}>{children}</AudioContext.Provider>;
+// };
+
+// export default AudioProvider; 
+
+import { createContext, useState } from "react";
+import tracksList from "../assets/tracksList";
+
+const defaultTrack = tracksList[0];
+
+const audio = new Audio(defaultTrack.src);
+
+export const AudioContext = createContext({});
+
+const AudioProvider = ({ children }) => {
+    const [currentTrack, setCurrentTrack] = useState(defaultTrack);
+    const [isPlaying, setPlaying] = useState(false);
+
+    const handleToggleAudio = (track) => {
+        if (currentTrack.id !== track.id) {
+            setCurrentTrack(track);
+            setPlaying(true);
+
+            audio.src = track.src;
+            audio.currentTime = 0;
+            audio.play();
+
+            return;
+        }
+
+        if (isPlaying) {
+            audio.pause();
+            setPlaying(false);
+        } else {
+            audio.play();
+            setPlaying(true);
+        }
+    };
+
+    const value = { audio, currentTrack, isPlaying, handleToggleAudio };
+
+    return (
+        <AudioContext.Provider value={value}>{children}</AudioContext.Provider>
+    );
+};
+
+export default AudioProvider;
